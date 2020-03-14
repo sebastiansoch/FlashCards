8ffa69fcb781c5bb1f34ff39ce1920e1ad3b40b1
Sebastian Soch <sebastian.soch@gmail.com>
2020-03-14 11:42:57 +0100

    -Thymeleaf -jQuery

--- src/main/java/com/gmail/soch/sebastian/flashcards/security/SecurityConfig.java
+++ src/main/java/com/gmail/soch/sebastian/flashcards/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
                 .formLogin()
                 .and()
                 .rememberMe()
